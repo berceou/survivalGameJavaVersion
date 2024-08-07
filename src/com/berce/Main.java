f466b0b2757245b9e822cb93bc498fcf48d94682
bouygun <berceozumm@gmail.com>
2024-08-07 10:37:56 +0300

    feat: empty commit

--- src/com/berce/Main.java
+++ src/com/berce/Main.java
@@ -11,7 +11,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         try{
             Scanner scanner = new Scanner(System.in);
             System.out.println("Pls enter the text input: ");
